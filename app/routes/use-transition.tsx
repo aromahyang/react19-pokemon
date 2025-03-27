752f318a0eb1f3d46ce11beafbb5cde2666153fc
aromahyang <aromahyang.dev@gmail.com>
2025-03-27 11:08:33 +0900

    :sparkles: useTransition 페이지 생성

---
+++ app/routes/use-transition.tsx
@@ -0,0 +1,5 @@
+import Page from "../use-transition/page";
+
+export default function UseTransition() {
+  return <Page />;
+}
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,7 +3,10 @@ import logoLight from "./logo-light.svg";
 
 export function Welcome() {
   return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    <main className="pt-16 pb-4">
+      <div>
+        <a href="/use-transition">Go to useTransition</a>
+      </div>
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
