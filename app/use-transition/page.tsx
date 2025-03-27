067448794f64cd16004c035829208abf72f90c75
aromahyang <aromahyang.dev@gmail.com>
2025-03-27 11:17:23 +0900

    :art: useTransition 적용

--- app/use-transition/page.tsx
+++ app/use-transition/page.tsx
@@ -1,3 +1,39 @@
+import { useState, useTransition } from "react";
+import fetch from "~/api";
+import { POKEMON_API_URL } from "~/api/constants";
+
 export default function Page() {
-  return <div>useTransition page</div>;
+  const [id, setId] = useState<number | null>(null);
+  const [pokemon, setPokemon] = useState<Record<string, any> | null>(null);
+  const [isPending, startTransition] = useTransition();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setId(() => Number(e.target.value));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    startTransition(async () => {
+      const res = await fetch({
+        url: `${POKEMON_API_URL}/pokemon/${id}`,
+        method: "get",
+      });
+      setPokemon(() => res);
+    });
+  };
+
+  return (
+    <div>
+      <h2>useTransition()</h2>
+      <p>id를 입력하여 정보 가져오기</p>
+      <form onSubmit={handleSubmit}>
+        <input type="number" onChange={handleInputChange} />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <p>pending 여부: {isPending ? "true" : "false"}</p>
+        <p>결과: {JSON.stringify(pokemon)}</p>
+      </div>
+    </div>
+  );
 }
