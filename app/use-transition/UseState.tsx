6a54762b2ef83ddccd8d2f188087db04825ca9cd
aromahyang <aromahyang.dev@gmail.com>
2025-04-11 14:52:51 +0900

    :art: useTransition 비교 코드 수정

---
+++ app/use-transition/UseState.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  isPending: boolean;
+  data: Record<string, any> | null;
+};
+
+export default function UseState({ isPending, data }: Props) {
+  return (
+    <div>
+      <p>pending 여부: {isPending ? "true" : "false"}</p>
+      <p>결과: {JSON.stringify(data?.name)}</p>
+    </div>
+  );
+}
--- app/use-transition/page.tsx
+++ app/use-transition/page.tsx
@@ -2,34 +2,42 @@ import { useState, useTransition } from "react";
 import fetch from "~/api";
 import { POKEMON_API_URL } from "~/api/constants";
 import { Input } from "~/components/ui";
+import UseState from "./UseState";
+import UseTransition from "./UseTransition";
 
 export default function Page() {
-  /* ------------------------------ No Transition ----------------------------- */
-  const [isPendingWithoutTransition, setIsPending] = useState(false);
+  /* ------------------------------ no transition ----------------------------- */
+  const [isPending1, setIsPending1] = useState(false);
   const [pokemon1, setPokemon1] = useState<Record<string, any> | null>(null);
   /* -------------------------------------------------------------------------- */
 
-  /* ------------------------------ useTransition ----------------------------- */
+  /* ------------------------------- transition ------------------------------- */
   const [pokemon2, setPokemon2] = useState<Record<string, any> | null>(null);
-  const [isPending, startTransition] = useTransition();
+  const [isPending2, startTransition] = useTransition();
   /* -------------------------------------------------------------------------- */
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    const { value } = event.target;
+    if (!value) return;
     (async () => {
-      setIsPending(true);
+      setIsPending1(() => true);
       const res = await fetch({
-        url: `${POKEMON_API_URL}/pokemon/${e.target.value}`,
+        url: `${POKEMON_API_URL}/pokemon/${value}`,
         method: "get",
       });
       setPokemon1(() => res);
-      setIsPending(false);
+      setIsPending1(() => false);
     })();
     startTransition(async () => {
       const res = await fetch({
-        url: `${POKEMON_API_URL}/pokemon/${e.target.value}`,
+        url: `${POKEMON_API_URL}/pokemon/${value}`,
         method: "get",
       });
-      setPokemon2(() => res);
+      startTransition(() => {
+        setPokemon2(() => res);
+      });
     });
   };
 
@@ -37,22 +45,23 @@ export default function Page() {
     <div className="p-4">
       <h2 className="font-bold text-2xl">useTransition()</h2>
       <p>
-        useTransition()은 UI를 차단하지 않고 상태를 업데이트할 수 있는 React hook이다.<br />
-        React에서 Transition은 긴급하지 않은 상태 업데이트를 의미한다.<br />
-        React가 특정 상태 업데이트를 즉시 처리하지 않고, 우선순위가 낮은 작업으로 간주하여 처리할 수 있게 해준다.<br />
+        useTransition()은 UI를 차단하지 않고 상태를 업데이트할 수 있는 React
+        hook이다.
+        <br />
+        React에서 Transition은 긴급하지 않은 상태 업데이트를 의미한다.
+        <br />
+        React가 특정 상태 업데이트를 즉시 처리하지 않고, 우선순위가 낮은
+        작업으로 간주하여 처리할 수 있게 해준다.
+        <br />
       </p>
-      <h3 className="mt-4 font-bold text-xl">id를 입력하여 포켓몬 이름 보여주기</h3>
+      <h3 className="mt-4 font-bold text-xl">
+        id를 입력하여 포켓몬 이름 보여주기
+      </h3>
       <Input type="number" onChange={handleInputChange} />
       <p className="mt-4">useTransition() 이용 X</p>
-      <div>
-        <p>pending 여부: {isPendingWithoutTransition ? "true" : "false"}</p>
-        <p>결과: {JSON.stringify(pokemon1?.name)}</p>
-      </div>
+      <UseState isPending={isPending1} data={pokemon1} />
       <p className="mt-4">useTransition() 이용</p>
-      <div>
-        <p>pending 여부: {isPending ? "true" : "false"}</p>
-        <p>결과: {JSON.stringify(pokemon2?.name)}</p>
-      </div>
+      <UseTransition isPending={isPending2} data={pokemon2} />
     </div>
   );
 }
